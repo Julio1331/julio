bb0102baa23418e702dc92ad239d95d5d874fbbf
Julio Pereyra <juliopereyrav@gmail.com>
2019-10-28 20:05:50 -0300

    primer commit

--- src/ejemplo1git.vhd
+++ src/ejemplo1git.vhd
@@ -1,1 +1,14 @@
 library ieee;
+use ieee.std_logic_1164.all;
+
+entity ejemplo1git is
+	port(
+			x1, x2: in std_logic_vector(3 downto 0);
+			displays: out std_logic_vector(13 downto 0) 
+			);
+end ejemplo1git;
+architecture bev of ejemplo1git
+
+begin
+--TODO
+end bev;
